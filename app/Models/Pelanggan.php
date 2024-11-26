2ac541580683bd5368abf998ab4b7b621c426d3f
Muhammad Daris Hafizh Permana <darishafizh2021@gmail.com>
2024-11-26 23:41:27 +0700

    Theme: Update

--- app/Models/Pelanggan.php
+++ app/Models/Pelanggan.php
@@ -8,9 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Pelanggan extends Model
 {
     use HasFactory;
-    protected $table = "data_pelanggan";
-    protected $primaryKey = "id_pelanggan";
-    protected $fillable = ["id_pelanggan", "nama_pelanggan", "alamat", "hp_pelanggan"];
-    public $timestamps = false;
-    protected $keyType = 'string';
 }
--- app/Models/Penjualan.php
+++ app/Models/Penjualan.php
@@ -8,24 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Penjualan extends Model
 {
     use HasFactory;
-    protected $table = "data_penjualan";
-    protected $primaryKey = "id_penjualan";
-    protected $fillable = ["id_penjualan", "tanggal_penjualan", "id_user", "id_produk", "jumlah", "total_harga", "id_pelanggan"];
-    public $timestamps = false;
-    protected $keyType = 'string';
-
-    public function produk()
-    {
-        return $this->belongsTo(Produk::class, 'id_produk', 'id_produk');
-    }
-
-    public function pelanggan()
-    {
-        return $this->belongsTo(Pelanggan::class, 'id_pelanggan', 'id_pelanggan');
-    }
-
-    public function user()
-    {
-        return $this->belongsTo(User::class, 'id_user', 'id_user');
-    }
 }
